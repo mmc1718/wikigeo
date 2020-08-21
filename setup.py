3ac513c858e457b4f26969546c676dcb767bfd00
Mary M <marymcguire1718@gmail.com>
2020-08-21 23:37:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,10 @@ setuptools.setup(
     author_email='marymcguire1718@gmail.com',
     install_requires=["requests_html",
     "googletrans", "fuzzywuzzy", "python-Levenshtein-wheels"],
-    extras_require={"pytest": "pytest==6.0.1"},
+    extras_require={"pytest": "pytest==6.0.1", "tox": "tox==3.19.0"},
     classifiers=[
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Wiki"
