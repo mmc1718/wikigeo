ea7573336e5d7a327c11a04f49e81ba92825f219
Mary M <marymcguire1718@gmail.com>
2020-08-21 22:28:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,19 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='wikigeo',
     version='0.0.1',
-    description='Supplementing geographic data with data on points of interest from wikipedia',
+    description='Getting point of interest data by geographic coordinates from Wikipedia',
     long_description=long_description,
     packages=setuptools.find_packages(),
     author='Mary McGuire',
     author_email='marymcguire1718@gmail.com',
     install_requires=["requests_html",
     "googletrans", "fuzzywuzzy", "python-Levenshtein-wheels"],
+    extras_require={"pytest": "pytest==6.0.1"},
     classifiers=[
         "Programming Language :: Python :: 3.8",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Wiki"
     ],
     url='https://github.com/mmc1718/wikigeo'
 )
