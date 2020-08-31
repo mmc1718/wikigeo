7825686c47687315e7cd2ed87457d20e4ecbceb5
Mary M <marymcguire1718@gmail.com>
2020-08-31 18:19:26 +0100

    adding to documentation and correcting missing filter
    adding to documentation strings. Adding minnamematch filter to get_name_match so that it actually filters on the minnamematch parameter

--- wikigeo/wikisearch.py
+++ wikigeo/wikisearch.py
@@ -9,19 +9,28 @@ import math
 
 class WikiExtractor(object):
 
-    """methods to search wikipedia and return specified information"""
+    """
+    
+    Search wikipedia and return specified information.
+    
+    """
 
     def __init__(self, language, userinfo):
         self.user = userinfo
         self.language = language
 
     def get_nearby_pages(self, lat, lon, limit=4, radiusmetres=10000):
-        """get requested info for all pages nearby given coordinates.
+        """
+        
+        Get details for all pages within a given radius of given coordinates.
 
         radiusmeters: the distance in metres to search, can be max 10000 (10km). default is 10000
+        
         limit: max number of pages to return (default 4)
 
-        returns list of dictionaries with title, label, description, coordinates, image"""
+        returns list of dictionaries with title, label, description, coordinates, image
+        
+        """
 
         titles = []
         labels = []
@@ -59,13 +68,20 @@ class WikiExtractor(object):
         
 
     def get_page_text(self, pagetitle, limit=False, translateto=False):
-        """scrape the full text of a given page
+        """
+        
+        Retrieve the full text of a given page by page title.
         
         pagetitle: exact title of page to scrape
-        limit: int, character limit of text returned
-        translateto: language code for language that text should be translated into
         
-        returns a dictionary with pagetitle and text
+        limit: int, character limit of text returned. If set to False then the full text is returned. (default is False).
+        
+        translateto: language code for language that text should be translated into.
+        
+        returns a dictionary with pagetitle and text.
+
+        Note: if requesting translated text, limit should be a max of 10000
+
         """
             
         wiki = WikiText(limit, self.language)
@@ -75,15 +91,23 @@ class WikiExtractor(object):
         return result
 
     def get_nearby_images(self, lat, lon, radiusmetres=10000, nametomatch=False, matchfilter=False):
-        """gets all images nearby given coordinates from wiki commons.
+        """
+        
+        Gets all images nearby given coordinates from Wikimedia Commons.
 
         radiusmetres: distance to search, max 10000 (10km)
-        nametomatch: if set to a name then results are given a match rating against the name
+        
+        nametomatch: if set to a string then results are given a match rating against the string. If False then no matching is done.
+        
         matchfilter: if set to an int then results are filtered for results that have a higher rated name match,
         can be between 0 (no matching) and 100 (exact match only)
         
         returns list of dictionaries containing image data
-        [{'image': i, 'title': t, 'url': url, 'name match': x}, ...]"""
+        [{'image': i, 'title': t, 'url': url, 'name match': x}, ...]
+        
+        Note: if using matchfilter, nametomatch must be set to a string
+        
+        """
 
         commons = WikiCommonsAPI(self.user)
         if(matchfilter and (not nametomatch)):
@@ -119,24 +143,34 @@ class WikiExtractor(object):
         distance = 6378 * math.acos(math.cos(latA) * math.cos(latB) * math.cos(lngB - lngA) + math.sin(latA) * math.sin(latB))
         return distance
 
-    def get_page_match(self, keyword, searchlat, searchlon, bestmatch, maxdistance=30, minnamematch=50):
-        """searches for all geolocated wiki pages containing given keyword
-        filters to results that are less than the maxdistance (default is 30km)
+    def get_page_match(self, keyword, searchlat, searchlon, bestmatch, maxdistance=30, minnamematch=0):
+        """
+        
+        Searches for all geolocated wiki pages containing given keyword.
+        Filters to results that are less than the maxdistance (default is 30km)
 
-        bestmatch: if 'name' will return the best (highest on scale 0-100) match based 
-                    on keyword matching page title
-                    if 'distance' will return the best (closest) match on distance
-                    if False will return all geolocated matches nearby
+        bestmatch: 
+
+        bestmatch='name' will return the best (highest on scale 0-100) match based 
+        on keyword matching page title 
+
+        bestmatch='distance' will return the best (closest) match on distance
+
+        bestmatch=False will return all geolocated matches nearby
+        
         maxdistance: int, results must be less than the max distance in km of results from search coords 
                     (default 30)
+        
         minnamematch: int, results must have a higher name match than minnamematch, between 0-100 
                     (default 50)
 
         returns a (list of) dictionaries containing page title, description, label, image, distance, coords
         and match rating
 
-        Note: Works best for unique, proper names of geographically located places.
-        Bases location on centrepoint, so points on the edge of very large/spread out entities may not match."""
+        Note: Works best for unique, proper names of geographically located places (e.g. landmarks, buildings, parks)
+        Bases location on centrepoint, so points on the edge of very large/spread out entities may not match.
+        
+        """
 
         data = []
         wiki = WikipediaAPI(self.user, self.language)
@@ -178,7 +212,7 @@ class WikiExtractor(object):
         logging.debug('results with coords saved from wiki search: ' + str(len(data)))
         # filtering for relevant results
         #results = [place for place in data if ((place['distance'] < 10 and place['name match'] > 50) or (place['name match'] > 65 and place['distance'] < 30) or (place['distance'] < 5))]
-        results = [place for place in data if (place['distance'] < maxdistance)]
+        results = [place for place in data if ((place['distance'] < maxdistance) and (place['name match'] > minnamematch))]
         if(results):
             # getting best match if requested
             if(bestmatch == 'name'):
