350f80161347d55a735df0d549b449b3e296864d
Mary M <marymcguire1718@gmail.com>
2020-09-02 22:36:15 +0100

    adding a test for breaking case in image search
    adding a test to check when no images are returned, this was giving a key error

--- test/test_wikisearch.py
+++ test/test_wikisearch.py
@@ -1,12 +1,23 @@
 import pytest
 from wikigeo import WikiExtractor
+from pprint import pprint
+import logging
+
 
 
 def test_get_nearby_images():
     wiki = WikiExtractor(language='en', userinfo="test")
     images = wiki.get_nearby_images(55.95527, -3.18108, nametomatch='Calton Hill', matchfilter=40)
     for result in images:
+        print('hello')
         assert isinstance(result, dict) and isinstance(result['image'], str)
+
+def test_get_nearby_images_none():
+    wiki = WikiExtractor(language='en', userinfo="test")
+    images = wiki.get_nearby_images(58.076026212, 1.730324014, nametomatch='Norwegian Boundary Sediment Plain', matchfilter=44)
+    for result in images:
+        print('result is ' + str(result))
+        assert isinstance(result, dict)
 
 def test_get_page_match():
     wiki = WikiExtractor(language='en', userinfo="test")
